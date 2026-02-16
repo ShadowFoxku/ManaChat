711b56efe933fb9bc5aa3f38c200ef57650e77ea
ShadowFoxku <rokutheshadowfox@gmail.com>
2026-02-16 17:34:08 +1300

    register DI stuff

--- ManaChat.API/Program.cs
+++ ManaChat.API/Program.cs
@@ -1,3 +1,11 @@
+using ManaChat.Core.Constants;
+using ManaChat.Identity.Constants;
+using ManaChat.Identity.Repositories;
+using ManaChat.Identity.Services;
+using ManaFox.Databases.Core.Interfaces;
+using ManaFox.Databases.Extensions;
+using ManaFox.Databases.TSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +13,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddRuneReaderDb(DatabaseConstants.MessagingDatabaseString, builder.Configuration.GetSection("ConnectionStrings"));
+builder.Services.AddScoped<IRuneReaderManager, RuneReaderManager>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 
 var app = builder.Build();
 
