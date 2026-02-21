1a4f365c61ab757dd766f7687739ebc9414128c7
ShadowFoxku <rokutheshadowfox@gmail.com>
2026-02-22 12:49:19 +1300

    rename procs for consistency

--- ManaChat.Identity/Constants/IdentityDBConstants.cs
+++ ManaChat.Identity/Constants/IdentityDBConstants.cs
@@ -2,22 +2,23 @@
 {
     public static class IdentityDBConstants
     {
+        private const string Schema = "identity";
         public static class StoredProcedures
         {
-            public const string GetUserById = "usp_get_user_by_id";
-            public const string GetUserByUsername = "usp_get_user_by_username";
-            public const string SaveUser = "usp_save_user";
-            public const string UpdateUserPassword = "usp_save_user_password";
-            public const string DeleteUser = "usp_delete_user";
+            public const string GetUserById = $"{Schema}.usp_user_get_by_id";
+            public const string GetUserByUsername = $"{Schema}.usp_user_get_by_username";
+            public const string SaveUser = $"{Schema}.usp_user_save";
+            public const string UpdateUserPassword = $"{Schema}.usp_user_save_password";
+            public const string DeleteUser = $"{Schema}.usp_user_delete";
 
-            public const string GetUserIdentitiesForUser = "usp_get_user_identities_for_user";
-            public const string GetUserIdentity = "usp_get_user_identity";
-            public const string SaveUserIdentity = "usp_save_user_identity";
-            public const string DeleteUserIdentity = "usp_delete_user_identity";
+            public const string GetUserIdentitiesForUser = $"{Schema}.usp_identities_get_for_user";
+            public const string GetUserIdentity = $"{Schema}.usp_identities_get";
+            public const string SaveUserIdentity = $"{Schema}.usp_identities_save";
+            public const string DeleteUserIdentity = $"{Schema}.usp_identities_delete";
 
-            public const string GetUserRelationships = "usp_get_user_relationships_for_user";
-            public const string GetUserRelationBetweenUsers = "usp_get_user_relationships_between_users";
-            public const string SaveUserRelationship = "usp_save_user_relationship";
+            public const string GetUserRelationships = $"{Schema}.usp_relationships_get_for_user";
+            public const string GetUserRelationBetweenUsers = $"{Schema}.usp_relationships_get_between_users";
+            public const string SaveUserRelationship = $"{Schema}.usp_relationships_save";
         }
     }
 }
